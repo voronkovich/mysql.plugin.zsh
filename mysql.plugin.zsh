3063252cd8c5436922d975cb9ac27591cd21b77f
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2014-10-02 18:07:36 +0400

    Concatenate all files in one.

--- mysql.plugin.zsh
+++ mysql.plugin.zsh
@@ -1,7 +1,86 @@
-export ZSH_PLUGIN_MYSQL_PATH=$(dirname $(readlink -f $0))
-
 alias my='mysql'
-alias mycu="$ZSH_PLUGIN_MYSQL_PATH/mysql-create-user"
-alias mycd="$ZSH_PLUGIN_MYSQL_PATH/mysql-create-db"
+alias mys='mysqlshow'
+alias mycu='mysql-create-user'
+alias mycud='mysql-create-user-and-database'
+alias mycd='mysql-create-db'
+alias mytd='mysql-truncate-database'
 alias mysu="mysql -e 'SELECT user, host FROM mysql.user'"
-alias mysd="mysql -e 'SHOW DATABASES'"
+
+# Create a new mysql user
+mysql-create-user() {
+    PROGNAME=${0##*/}
+
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: $PROGNAME user_name [password]"
+        return 1
+    fi
+
+    USER=$1
+    if [[ $(echo $USER | grep -c '@') -eq 0 ]]; then
+        USER="$USER@localhost"
+    fi
+
+    PASSWORD=$2
+    if [[ -z $PASSWORD ]]; then
+        echo -n "Enter user password: \n"; read -s PASSWORD
+    fi
+
+    PASSWORD_HASH=$(mysql -e "SELECT PASSWORD('$PASSWORD')" | grep '\*\w*')
+
+    CREATE_USER_QUERY="CREATE USER \`${USER/@/\`@\`}\` IDENTIFIED BY PASSWORD '$PASSWORD_HASH'"
+
+    mysql -e $CREATE_USER_QUERY
+}
+
+# Create a new mysql database
+mysql-create-db() {
+    PROGNAME=${0##*/}
+
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: $PROGNAME database_name [user_name]"
+        return 1
+    fi
+
+    DATABASE=$1
+    USER=$2
+    CHARACTER_SET=${CHARACTER_SET=utf8}
+    COLLATION=${COLLATION=utf8_general_ci}
+
+    CREATE_DATABASE_QUERY="CREATE DATABASE \`$DATABASE\` CHARACTER SET $CHARACTER_SET COLLATE $COLLATION"
+
+    if [[ -n $USER ]]; then
+        if [[ $(echo $USER | grep -c '@') -eq 0 ]]; then
+            USER="$USER@localhost"
+        fi
+        CREATE_DATABASE_QUERY="$CREATE_DATABASE_QUERY; GRANT ALL PRIVILEGES ON \`$DATABASE\`.* TO \`${USER/@/\`@\`}\`"
+    fi
+
+    mysql -e $CREATE_DATABASE_QUERY
+}
+
+# Create a new user and database with the same name
+mysql-create-user-and-database() {
+    PROGNAME=${0##*/}
+
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: $PROGNAME user_name [password]"
+        return 1
+    fi
+
+    $ZSH_PLUGIN_MYSQL_PATH/mysql-create-user $*
+    $ZSH_PLUGIN_MYSQL_PATH/mysql-create-db $1 $1
+}
+
+# Truncate all tables in a database
+mysql-truncate-database() {
+    PROGNAME=${0##*/}
+
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: $PROGNAME database"
+        return 1
+    fi
+
+    for table in $(mysql -Nse 'SHOW TABLES' $1); do
+        mysql -e "TRUNCATE TABLE \`$table\`" $1
+    done
+}
